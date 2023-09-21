10ebf7e240aed8a9f48ab79216982602e77170fb
Maxohayce <devmax.corl@gmail.com>
2023-09-21 23:12:38 +0100

    updated changes

--- src/constants/imageData.js
+++ src/constants/imageData.js
@@ -9,49 +9,121 @@ import pexels8 from "../assets/pexels8.jpg";
 
 const ImageData = [
   {
-    id: 11,
-    title: "wick",
-    img: "https://s3-alpha-sig.figma.com/img/154e/382f/b7e8be84d7248b8b9812172cad1cd0c9?Expires=1696204800&Signature=k5dko53aDmGzi~SxURI96anjShdJ0YQ~NVOV-Z~FIBtSFU9n8-u3NITILCzbaz0~8jQfgDK7fC3~-Ldjlftq~SdwZopF~MFhzqO83ERW8ZaVBkZJWhzA42PHV4DRXvqwuL~ewLMVu~fOV-ckeBaqG-VfO1~xIFaVqNtyxXHxlHAcpG3MqoArabFW4M0ZabXlXNmsECGgHTJkjAYQ9f8nlAD1m7tWjt2aU~Kn6t9xBqjS80eAuM9D6PqcITFKIpGJm-L42hp7I2GrUX9k~y2a3LejepXetuj4S~SvSZkPMqlZ3ZZ9oA7qEyvu-AGtfqsXmr0b9DNPhwBtk98iVRezCw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    title: "First Column",
+    items: [
+      {
+        id: "bhaubda",
+        title: "wick",
+        img: "https://s3-alpha-sig.figma.com/img/154e/382f/b7e8be84d7248b8b9812172cad1cd0c9?Expires=1696204800&Signature=k5dko53aDmGzi~SxURI96anjShdJ0YQ~NVOV-Z~FIBtSFU9n8-u3NITILCzbaz0~8jQfgDK7fC3~-Ldjlftq~SdwZopF~MFhzqO83ERW8ZaVBkZJWhzA42PHV4DRXvqwuL~ewLMVu~fOV-ckeBaqG-VfO1~xIFaVqNtyxXHxlHAcpG3MqoArabFW4M0ZabXlXNmsECGgHTJkjAYQ9f8nlAD1m7tWjt2aU~Kn6t9xBqjS80eAuM9D6PqcITFKIpGJm-L42hp7I2GrUX9k~y2a3LejepXetuj4S~SvSZkPMqlZ3ZZ9oA7qEyvu-AGtfqsXmr0b9DNPhwBtk98iVRezCw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+        imgTag: "tag1",
+      },
+      {
+        id: "akf88vn",
+        title: "twelve",
+        img: pexels1,
+        imgTag: "tag2",
+      },
+      {
+        id: "ca83nd",
+        title: "thirteen",
+        img: pexels2,
+        imgTag: "tag3",
+      },
+      {
+        id: "0fnf83d",
+        title: "fourteen",
+        img: pexels3,
+        imgTag: "tag4",
+      },
+    ],
+  },
+
+  {
+    title: "Second Column",
+    items: [
+      {
+        id: "aknaf98",
+        title: "wick",
+        img: "https://s3-alpha-sig.figma.com/img/154e/382f/b7e8be84d7248b8b9812172cad1cd0c9?Expires=1696204800&Signature=k5dko53aDmGzi~SxURI96anjShdJ0YQ~NVOV-Z~FIBtSFU9n8-u3NITILCzbaz0~8jQfgDK7fC3~-Ldjlftq~SdwZopF~MFhzqO83ERW8ZaVBkZJWhzA42PHV4DRXvqwuL~ewLMVu~fOV-ckeBaqG-VfO1~xIFaVqNtyxXHxlHAcpG3MqoArabFW4M0ZabXlXNmsECGgHTJkjAYQ9f8nlAD1m7tWjt2aU~Kn6t9xBqjS80eAuM9D6PqcITFKIpGJm-L42hp7I2GrUX9k~y2a3LejepXetuj4S~SvSZkPMqlZ3ZZ9oA7qEyvu-AGtfqsXmr0b9DNPhwBtk98iVRezCw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+        imgTag: "tag5",
+      },
+      {
+        id: "8evmcnh",
+        title: "twelve",
+        img: pexels4,
+        imgTag: "tag6",
+      },
+      {
+        id: "mnvhvr",
+        title: "thirteen",
+        img: pexels5,
+        imgTag: "tag7",
+      },
+      {
+        id: "i9dnbh",
+        title: "fourteen",
+        img: pexels6,
+        imgTag: "tag8",
+      },
+    ],
   },
   {
-    id: 12,
-    title: "leaves",
-    img: pexels1,
+    title: "Third Column",
+    items: [
+      {
+        id: "aknajf",
+        title: "wick",
+        img: "https://s3-alpha-sig.figma.com/img/154e/382f/b7e8be84d7248b8b9812172cad1cd0c9?Expires=1696204800&Signature=k5dko53aDmGzi~SxURI96anjShdJ0YQ~NVOV-Z~FIBtSFU9n8-u3NITILCzbaz0~8jQfgDK7fC3~-Ldjlftq~SdwZopF~MFhzqO83ERW8ZaVBkZJWhzA42PHV4DRXvqwuL~ewLMVu~fOV-ckeBaqG-VfO1~xIFaVqNtyxXHxlHAcpG3MqoArabFW4M0ZabXlXNmsECGgHTJkjAYQ9f8nlAD1m7tWjt2aU~Kn6t9xBqjS80eAuM9D6PqcITFKIpGJm-L42hp7I2GrUX9k~y2a3LejepXetuj4S~SvSZkPMqlZ3ZZ9oA7qEyvu-AGtfqsXmr0b9DNPhwBtk98iVRezCw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+        imgTag: "tag9",
+      },
+      {
+        id: "akn8wf",
+        title: "twelve",
+        img: pexels7,
+        imgTag: "tag10",
+      },
+      {
+        id: "akm28f",
+        title: "thirteen",
+        img: pexels8,
+        imgTag: "tag11",
+      },
+      {
+        id: "adnq9d8",
+        title: "fourteen",
+        img: pexels1,
+        imgTag: "tag12",
+      },
+    ],
   },
   {
-    id: 13,
-    title: "wick",
-    img: pexels2,
-  },
-  {
-    id: 14,
-    title: "wick",
-    img: pexels3,
-  },
-  {
-    id: 15,
-    title: "wick",
-    img: pexels4,
-  },
-  {
-    id: 16,
-    title: "wick",
-    img: pexels5,
-  },
-  {
-    id: 17,
-    title: "wick",
-    img: pexels6,
-  },
-  {
-    id: 18,
-    title: "wick",
-    img: pexels7,
-  },
-  {
-    id: 18,
-    title: "wick",
-    img: pexels8,
+    title: "Fourth Column",
+    items: [
+      {
+        id: "0398nb",
+        title: "wick",
+        img: "https://s3-alpha-sig.figma.com/img/154e/382f/b7e8be84d7248b8b9812172cad1cd0c9?Expires=1696204800&Signature=k5dko53aDmGzi~SxURI96anjShdJ0YQ~NVOV-Z~FIBtSFU9n8-u3NITILCzbaz0~8jQfgDK7fC3~-Ldjlftq~SdwZopF~MFhzqO83ERW8ZaVBkZJWhzA42PHV4DRXvqwuL~ewLMVu~fOV-ckeBaqG-VfO1~xIFaVqNtyxXHxlHAcpG3MqoArabFW4M0ZabXlXNmsECGgHTJkjAYQ9f8nlAD1m7tWjt2aU~Kn6t9xBqjS80eAuM9D6PqcITFKIpGJm-L42hp7I2GrUX9k~y2a3LejepXetuj4S~SvSZkPMqlZ3ZZ9oA7qEyvu-AGtfqsXmr0b9DNPhwBtk98iVRezCw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+        imgTag: "tag13",
+      },
+      {
+        id: "jaduau",
+        title: "twelve",
+        img: pexels2,
+        imgTag: "tag14",
+      },
+      {
+        id: "bauhqe7",
+        title: "thirteen",
+        img: pexels3,
+        imgTag: "tag15",
+      },
+      {
+        id: "a87fbjh",
+        title: "fourteen",
+        img: pexels4,
+        imgTag: "tag16",
+      },
+    ],
   },
 ];
 
--- src/gallery/gallery.jsx
+++ src/gallery/gallery.jsx
@@ -1,126 +1,154 @@
-import React, { useRef, useState } from 'react';
-import "./gallery.css";
+import { useRef, useState, useEffect } from 'react';
+import './gallery.css';
 
-import ImageData from "../constants/imageData";
+import ImageData from '../constants/imageData';
 import Card from '../card/card';
 
-
 const Gallery = () => {
-    const [list, setList] = useState(data)
-    const [images, setImages] = React.useState(ImageData);
+    const [list, setList] = useState(ImageData);
     const [dragging, setDragging] = useState(false);
+    const [searchInput, setSearchInput] = useState('');
+    const [filteredImages, setFilteredImages] = useState([]);
 
     const dragItem = useRef();
     const dragNode = useRef();
 
+    useEffect(() => {
+        // Update filtered images whenever the list changes
+        if (searchInput.length > 0) {
+            const filtered = list.filter((group) => {
+                return group.items.some((image) => {
+                    return image.imgTag.toLowerCase().includes(searchInput.toLowerCase());
+                });
+            });
+            setFilteredImages(filtered);
+        } else {
+            setFilteredImages([]);
+        }
+    }, [list, searchInput]);
+
+    const handleDragEnter = (e, params) => {
+        console.log('Entering drag...');
+        const currentItem = dragItem.current;
+        if (e.target !== dragNode.current) {
+            console.log('target is not the same');
+            setList((oldList) => {
+                const newList = JSON.parse(JSON.stringify(oldList));
+                const { grp1, img1 } = params;
+                const { grp1: currentGroup, img1: currentImage } = currentItem;
+                const imageToMove = newList[currentGroup].items[currentImage];
+
+                newList[currentGroup].items.splice(currentImage, 1);
+                newList[grp1].items.splice(img1, 0, imageToMove);
+
+                dragItem.current = params;
+                return newList;
+            });
+        }
+    };
+
     const handleDragStart = (e, params) => {
-        console.log("drag starting...", params)
+        console.log('drag starting...', params);
         dragItem.current = params;
         dragNode.current = e.target;
-        dragNode.current.addEventListener("dragend", handleDragStartEnd)
+        dragNode.current.addEventListener('dragend', handleDragEnd);
         setTimeout(() => {
             setDragging(true);
         }, 0);
-    }
-
-    const handleDragEnter = (e, params) => {
-        console.log("Entering drag...")
-        const currentItem = dragItem.current;
-        if (e.target !== dragNode.current) {
-            console.log("target is not the same")
-            setList(oldList => {
-                let newList = JSON.parse(JSON.stringify(oldList));
-                newList[params.grp1].items.splice(params.item1, 0, newList[currentItem.grp1].items.splice(currentItem.item1, 1)(0))
-                dragItem.current = params
-                return newList
-            })
-        }
-    }
+    };
 
     const handleDragEnd = () => {
-        console.log("ending drag")
-        setDragging(false)
-        dragNode.current.removeEventListener("dragend", handleDragEnd);
+        console.log('ending drag');
+        setDragging(false);
+        dragNode.current.removeEventListener('dragend', handleDragEnd);
         dragItem.current = null;
         dragNode.current = null;
-    }
-
+    };
 
     const getStyles = (params) => {
         const currentItem = dragItem.current;
-        if (currentItem.grp1 === params.grp1 && currentItem.Item1 === params.item1) {
-            return "current dnd-item"
+        if (currentItem && currentItem.grp1 === params.grp1 && currentItem.img1 === params.img1) {
+            return 'current dnd-item';
         }
-        return "dnd-item"
-    }
+        return 'dnd-item';
+    };
+
+    const handleSearch = (e) => {
+        setSearchInput(e.target.value);
+    };
 
     return (
         <div className="gallery">
-            <div className="dnd-group" onDragEnter={dragging && !grp.items.length ? (e) => handleDragEnter(e, { grp1, item1: 0 })}>
-                <h4>First Column</h4>
-                {images.map((image, index) => (
-
-                    <div
-                        draggable
-                        onDragStart={(e) => { handleDragStart(e) }}
-                        // onDragEnd={(e) => {handleDragEnd(e,)} }
-                        onDragEnter={dragging ? (e) => { handleDragEnter(e, { grp1, item1 }) } : null}
-                        key={image.id}
-                        className={dragging ? getStyles() : "dnd-item"}>
-                        <Card
-                            src={image.img}
-                            title={image.title}
-                            id={image.id}
-                            index={index}
-                        />
-                    </div>
-                ))}
+            <input
+                className="search"
+                value={searchInput}
+                onChange={handleSearch}
+                type="text"
+                placeholder="Search for tag"
+            />
+            <div className="dnd-area">
+                {(searchInput.length === 0 || filteredImages.length === 0) ? (
+                    list.map((grp, grp1) => (
+                        <div
+                            key={grp.title}
+                            className="dnd-group"
+                            onDragEnter={
+                                dragging && !grp.items.length
+                                    ? (e) => handleDragEnter(e, { grp1, img1: 0 })
+                                    : null
+                            }
+                        >
+                            <h4>{grp.title}</h4>
+                            {grp.items?.map((image, img1) => (
+                                <div
+                                    draggable
+                                    onDragStart={(e) => {
+                                        handleDragStart(e, { grp1, img1 });
+                                    }}
+                                    onDragEnter={
+                                        dragging ? (e) => handleDragEnter(e, { grp1, img1 }) : null
+                                    }
+                                    key={grp.title + image.id}
+                                    className={dragging ? getStyles({ grp1, img1 }) : 'dnd-item'}
+                                >
+                                    <Card src={image.img} title={image.title} id={image.id} />
+                                </div>
+                            ))}
+                        </div>
+                    ))
+                ) : (
+                    filteredImages.map((grp, grp1) => (
+                        <div
+                            key={grp.title}
+                            className="dnd-group"
+                            onDragEnter={
+                                dragging && !grp.items.length
+                                    ? (e) => handleDragEnter(e, { grp1, img1: 0 })
+                                    : null
+                            }
+                        >
+                            <h4>{grp.title}</h4>
+                            {grp.items?.map((image, img1) => (
+                                <div
+                                    draggable
+                                    onDragStart={(e) => {
+                                        handleDragStart(e, { grp1, img1 });
+                                    }}
+                                    onDragEnter={
+                                        dragging ? (e) => handleDragEnter(e, { grp1, img1 }) : null
+                                    }
+                                    key={image.id}
+                                    className={dragging ? getStyles({ grp1, img1 }) : 'dnd-item'}
+                                >
+                                    <Card src={image.img} title={image.title} id={image.id} />
+                                </div>
+                            ))}
+                        </div>
+                    ))
+                )}
             </div>
-            <div className="dnd-group">
-                <h4>Second Column</h4>
-                {images.map((image, index) => (
-
-                    <div key={image.id} className="dnd-item">
-                        <Card
-                            src={image.img}
-                            title={image.title}
-                            id={image.id}
-                            index={index}
-                        />
-                    </div>
-                ))}
-            </div>
-            <div className="dnd-group">
-                <h4>Third Column</h4>
-                {images.map((image, index) => (
-
-                    <div key={image.id} className="dnd-item">
-                        <Card
-                            src={image.img}
-                            title={image.title}
-                            id={image.id}
-                            index={index}
-                        />
-                    </div>
-                ))}
-            </div>
-            <div className="dnd-group">
-                <h4>Fourth Column</h4>
-                {images.map((image, index) => (
-
-                    <div key={image.id} className="dnd-item">
-                        <Card
-                            src={image.img}
-                            title={image.title}
-                            id={image.id}
-                            index={index}
-                        />
-                    </div>
-                ))}
-            </div>
-
         </div>
-    )
-}
+    );
+};
 
 export default Gallery;
